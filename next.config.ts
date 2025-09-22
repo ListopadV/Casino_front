f73ec74fdda19259d9f26f8ae91dd0fc437f338c
ListopadV <vladlistopad6@gmail.com>
2025-09-22 16:47:36 +0200

    build

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
 
 
-  
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ["localhost"],
      remotePatterns: [
